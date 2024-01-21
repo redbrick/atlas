b7aab8a19bde9e853d39c0a1f5f43b16f93ebdb3
Jed Hazaymeh <jed.hazaymeh2@mail.dcu.ie>
2024-01-21 22:40:52 +0000

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('@redbrick/design-system/tailwind')],
+  content: ['./src/**/*.{html,md,11ty.js,liquid,njk}'],
+}
