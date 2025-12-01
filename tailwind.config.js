681ed2ebec69302c6de427ea72693c643b910a5e
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-12-01 22:12:43 +0000

    Made the Website more festive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,17 @@ module.exports = {
   presets: [require('@redbrick/design-system/tailwind')],
   content: ['./src/**/*.{html,md,njk,js,css}', './utils/**/*.js'],
   plugins: [require('@xpd/tailwind-3dtransforms')],
+  theme: {
+    extend: {
+      animation: {
+        fall: 'fall 5s linear infinite',
+      },
+      keyframes: {
+        fall: {
+          '0%': { transform: 'translateY(-10%)', opacity: '1' },
+          '100%': { transform: 'translateY(110%)', opacity: '0' },
+        },
+      },
+    },
+  }
 }
