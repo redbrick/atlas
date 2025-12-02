417631565630d448facb1129778d3dbb86803bf3
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-12-02 00:05:56 +0000

    Improved the snow falling with more randomization

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,23 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        fall: 'fall 5s linear infinite',
+        fall: 'fall 8s linear infinite',
+        'fall-soft-blink': 'fall-soft-blink 14s linear infinite', // Merged animation
       },
       keyframes: {
         fall: {
-          '0%': { transform: 'translateY(-10%)', opacity: '1' },
-          '100%': { transform: 'translateY(110%)', opacity: '0' },
+          '0%': { transform: 'translateY(-10vh)', opacity: '0' },
+          '10%': { opacity: '1' },
+          '100%': { transform: 'translateY(110vh)', opacity: '0' },
+        },
+        'fall-soft-blink': {
+          '0%': { transform: 'translateX(-1vw) translateY(-10vh)', opacity: '0.3' },
+          '10%': { transform: 'translateX(-0.5vw) translateY(5vh)', opacity: '1' },
+          '30%': { transform: 'translateX(0vw) translateY(30vh)', opacity: '0.5' },
+          '50%': { transform: 'translateX(0.5vw) translateY(50vh)', opacity: '1' },
+          '70%': { transform: 'translateX(1vw) translateY(70vh)', opacity: '0.4' },
+          '90%': { transform: 'translateX(0.5vw) translateY(90vh)', opacity: '1' },
+          '100%': { transform: 'translateX(-1vw) translateY(130vh)', opacity: '0.3' },
         },
       },
     },
